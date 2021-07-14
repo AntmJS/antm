0d0537fd1b5779f951da11cdc1ddacc807be0e25
三少 <sanshao@dian.so>
2021-07-14 21:38:31 +0800

    feat: 添加eslint和stylelint规范

---
+++ packages/eslint/index.js
@@ -0,0 +1,42 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-duplicate-imports': ['error'],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'type',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+        ],
+      },
+    ],
+  },
+}
---
+++ packages/stylelint/README.md
@@ -0,0 +1,19 @@
+# @antmjs/stylelint
+
+> 样式统一规范工具库
+
+## 安装
+
+```bash
+yarn add @antmjs/stylelint --dev
+```
+
+## 使用
+
+stylelint.config.js
+
+```javascript
+module.exports = {
+  extends: ['@antmjs/stylelint']
+}
+```
