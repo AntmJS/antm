ecc2aaceeb9f70782ebcb8174c3826495f052d78
三少 <sanshao@dian.so>
2021-07-14 16:42:23 +0800

    style: ts异常修复

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Hulk（绿巨人）
 
 > 前端工程应用工具集
+
+- @hulkjs/preset-env
--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: [
     [
-      '@hulkjs/babel-preset',
+      'packages/babel-preset',
       {
         presets: {
           env: {
