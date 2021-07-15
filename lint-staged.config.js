9d2aa8422f6feeb7da2a4f631980e22461b83e54
三少 <sanshao@dian.so>
2021-07-15 20:56:52 +0800

    fix: 修复stylelint校验时报错

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   '**/*.{js,jsx,ts,tsx}': ['npx eslint -c eslint.config.js --fix'],
   '**/*.ts?(x)': () => 'npx tsc -p tsconfig.json',
-  '**/*.{css,less}': ['npx stylelint --config stylelint.config.js --fix'],
+  '**/*.{css,less}': ['npx stylelint --aei --config stylelint.config.js --fix'],
 }
