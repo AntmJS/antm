9bdfef65edd74b009a0395987685231e92df8579
三少 <sanshao@dian.so>
2021-07-30 10:17:05 +0800

    chore: 命令行添加skipLibCheck

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   '**/*.{js,jsx,ts,tsx}': ['npx eslint -c eslint.config.js --fix'],
-  '**/*.ts?(x)': () => 'npx tsc -p tsconfig.json',
+  '**/*.ts?(x)': () => 'npx tsc -p tsconfig.json --skipLibCheck',
   '**/*.{css,less}': ['npx stylelint --aei --config stylelint.config.js --fix'],
 }
