d5e921e6ded5d2c2f955514e8a97df000eb34c5b
三少 <sanshao@dian.so>
2021-08-11 17:27:26 +0800

    feat: 添加eslint规则&添加其他小程序支持&更新weuidemo

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -20,6 +20,7 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-duplicate-imports': ['error'],
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     'react/jsx-uses-react': 'off',
