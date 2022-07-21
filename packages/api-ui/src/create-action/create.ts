ecac87e83fafd278e8867a067dd8b7a165ca79f0
kongjing@dian.so <apple@AppledeMacBook-Pro-2.local>
2022-07-21 18:35:50 +0800

    feat: update action

--- packages/api-ui/src/create-action/create.ts
+++ packages/api-ui/src/create-action/create.ts
@@ -9,8 +9,7 @@ import getConfig from '../config/getConfig.js'
 
 const CWD = process.cwd()
 const antmConfig = getConfig()
-const { requestImport, requestFnName, requestSuffix } =
-  antmConfig?.apiUi?.action || {}
+const { requestImport, requestFnName } = antmConfig?.apiUi?.action || {}
 
 type Iprops = {
   path: string
@@ -39,7 +38,6 @@ export default function createAll(props: Iprops) {
             fileName: fileName,
             requestImport,
             requestFnName,
-            requestSuffix,
           })
         } else {
           content = antmConfig?.apiUi?.action?.createDefaultModel(
@@ -67,18 +65,21 @@ export function createDefaultModel({
   requestFnName = 'createFetch',
   fileName = 'a',
   data = {},
-  requestSuffix = 'Action',
 }) {
   const packages = []
   let requestActionsStr = ''
 
   for (const key in data) {
-    if (key !== 'Record<string,any>') {
-      const item = data[key]
+    const item = data[key]
+    if (key !== 'Record<string,any>' && item.url) {
       packages.push(key)
       requestActionsStr += `
       // ${item.description}
-      export const ${key}${requestSuffix} = ${requestFnName}<${key}['request'], ${key}['response']>('${item.url}', '${item.method}');
+      export const ${key}${wordFirstBig(
+        fileName,
+      )} = ${requestFnName}<${key}['request'], ${key}['response']>('${
+        item.url
+      }', '${item.method}');
       `
     }
   }
@@ -86,10 +87,15 @@ export function createDefaultModel({
   const packagesStr = packages.join(',')
 
   return `
+  /* eslint-disable import/no-cycle */
   // @ts-nocheck
   ${requestImport}
   import type { ${packagesStr} } from './types/${fileName}';
 
   ${requestActionsStr}
   `
+}
+
+function wordFirstBig(str: string) {
+  return str.substring(0, 1).toLocaleUpperCase() + str.substring(1)
 }
