8c8942c86bc999c8a9e656473c8b026b8eae686c
三少 <sanshao@dian.so>
2022-08-09 17:35:57 +0800

    fix: 修复变量未引入的问题

--- packages/plugin-global-fix/index.js
+++ packages/plugin-global-fix/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { Compilation, sources } = require('webpack')
 const PLUGIN_NAME = 'GlobalFixPlugin'
 function GlobalFixPlugin(options) {
   this.options = options
