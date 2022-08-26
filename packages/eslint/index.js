e2d7fcdff86f7046562b1fd3965c0b4e857865bf
三少 <sanshao@dian.so>
2022-08-26 19:41:08 +0800

    fix: 关闭no-non-null-assertion

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -27,6 +27,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
